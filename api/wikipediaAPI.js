3708c9476e32a0107172085c70e24f2071478f6c
nik2143 <44278678+nik2143@users.noreply.github.com>
2021-09-05 18:52:31 +0200

    Remove puppeteer use from wikipedia command

--- api/wikipediaAPI.js
+++ api/wikipediaAPI.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
-const puppeteer = require('puppeteer');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const searchApiUrl = "https://it.wikipedia.org/w/api.php?action=opensearch&format=json&smaxage=0&uselang=it&search=%search%&namespace=0&limit=8&profile=engine_autoselect";
 
 
@@ -17,6 +18,34 @@ module.exports.getSearchResults = async function getSearchResults(tosearch){
 };
 
 module.exports.getInfo = async function getInfo(pageTitle){
+  return new Promise(async (resolve,reject)=>{
+    try {
+      let info = [];
+      let gotinfo = [];
+      await fetch(getUrl(pageTitle))
+      .then(response => response.text())
+      .then(text => {
+        const dom = new JSDOM(text);
+        const arr = dom.window.document.querySelector("#mw-content-text > div.mw-parser-output").childNodes;
+        let parts = [];
+        for (let el of arr) {
+          if (el.tagName=="P") {
+            parts.push(el);
+          }
+          if (el.className=="toc") break
+        }
+        parts.forEach(textpart=>gotinfo.push(textpart.textContent));
+        info = gotinfo.join("\n");
+      })
+      info = info.replace(/(\[\d+\])/g,"");
+      return resolve(info);
+    } catch (e) {
+        return reject(e);
+    }
+  })
+};
+
+/*module.exports.getInfo = async function getInfo(pageTitle){
   return new Promise(async (resolve,reject)=>{
     try {
       const browser = await puppeteer.launch();
@@ -42,7 +71,7 @@ module.exports.getInfo = async function getInfo(pageTitle){
         return reject(e);
     }
   })
-};
+};*/
 
 function getUrl(pageTitle){
   pageTitle = pageTitle.replace(/ /g, '_');
