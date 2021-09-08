b0be39eceecde6b2907748ab6556a043703ce5be
nik2143 <44278678+nik2143@users.noreply.github.com>
2021-09-08 02:19:20 +0200

    Allow to set a channel for memes

--- slashCommands/meme.js
+++ slashCommands/meme.js
@@ -1,16 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const { getImage } = require('random-reddit')
-
+const { getImage } = require('random-reddit');
+const config = require("../config.json");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('meme')
 		.setDescription('Invia meme random'),
 	async execute(interaction) {
+        if (interaction.channelId !== config.memeChannel){
+          return interaction.reply({ content: "Questo comando può essere usato solo nel canale meme", ephemeral: true});
+        }
         await interaction.deferReply();
-        const meme = await getImage('memesITA')
+        var tries = 0;
+        async function getMeme(){
+          try{
+            return await getImage('memesITA');
+          } catch (error) {
+            if (tries<3){
+              tries++;
+              getMeme();
+            } else {
+              throw(error);
+            }
+          }
+        }
+        var meme = await getMeme();
         return interaction.editReply({files: [{attachment: meme}]});
-        
 	},
 };
